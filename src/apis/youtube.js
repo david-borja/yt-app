674cba94988ce30d1dde7ee157a72cf7ad9ac4f7
david-borja <davidborjamartinez@gmail.com>
2021-07-07 17:37:06 +0200

    moved youtube key into .env file

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyCXx0po9NS8K5ijGU2SOU5-Gcg-HtRNNFA';
-
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
         type: 'video',
         maxResults: 5,
-        key: KEY
+        key: process.env.REACT_APP_YOUTUBE_KEY
     }
 });
